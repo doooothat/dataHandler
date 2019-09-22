8f9957f7b190b30fbdd7b2d5037d548ce9b0e6e0
smylere <caesitas@gmail.com>
2019-09-22 12:19:23 +0900

    .

--- app/app.R
+++ app/app.R
@@ -4,8 +4,10 @@ library(ggplot2)
 
 path = getwd()
 pathShiny = "/src/shiny"
+pathUtil = "/src/util"
 
 source(paste0(path,pathShiny,"/server/server.R"))
 source(paste0(path,pathShiny,"/ui/ui.R"))
+source(paste0(path,pathUtil,"/dbUtils.R"))
 
 shinyApp(ui = ui, server = server)
